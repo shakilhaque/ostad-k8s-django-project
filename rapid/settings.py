59370dda29570a04f515cd67c0e6da4347c68650
shakilhaque <shakilhaque29@gmail.com>
2025-05-15 00:11:25 +0600

    first commit

--- rapid/settings.py
+++ rapid/settings.py
@@ -12,13 +12,16 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 import os
 from pathlib import Path
-
 import environ
 
-env = environ.Env()
-environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Set up environ
+env = environ.Env()
+
+# Load .env file from BASE_DIR
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
